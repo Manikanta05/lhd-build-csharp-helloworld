c6e1d79ceb5fc50ca398d1e1cfc15bc78e48b978
Manikanta05 <manikantabalasubramanya47@gmail.com>
2021-01-11 20:29:45 +0530

    Added files

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Thank you Aditya");
+            var name ="Manikanta";
+            Console.WriteLine($"I am {name}");
+        }
+    }
+}
